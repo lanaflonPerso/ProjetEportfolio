5949e2babb9e8d76b9094adccfd18f12bbe972ee
vianneyba <vianneyba@free.fr>
2018-07-10 22:16:44 +0200

    modification du routage

--- init.sql
+++ init.sql
@@ -8,6 +8,7 @@ CREATE TABLE Utilisateurs (
 	Prenom				VARCHAR(20)		NOT NULL,
 	Adresse				VARCHAR(150)	NULL,
 	DateNaissance		DATE 			NULL,
+	motDePasse			VARCHAR(56)		NULL,
 	IsStagiaire 		TINYINT(1)		NULL,
 	IsAdministrateur	TINYINT(1)		NULL
 ) ENGINE = InnoDB;
@@ -16,6 +17,12 @@ INSERT INTO Utilisateurs(Nom, Prenom, Adresse, DateNaissance, IsStagiaire, IsAdm
 				VALUES ("Vianney", "Bailleux", "31 Allée du gros chêne 59320 haubourdin", "1979-06-07", 0, 1);
 INSERT INTO Utilisateurs(Nom, Prenom, Adresse, DateNaissance, IsStagiaire, IsAdministrateur)
 				VALUES ("Bruce", "Dickinson", "26 rue jean jaurés 59320 haubourdin", "1984-01-20", 1, 0);
+INSERT INTO Utilisateurs(Nom, Prenom, Adresse, DateNaissance, IsStagiaire, IsAdministrateur)
+				VALUES ("Rob", "Halford", "72, rue du Château 97480 SAINT-JOSEPH", "1944-07-23", 1, 0);
+INSERT INTO Utilisateurs(Nom, Prenom, Adresse, DateNaissance, IsStagiaire, IsAdministrateur)
+				VALUES ("Josette", "Allain", "26, rue des Chaligny 58000 NEVERS", "1967-06-16", 1, 0);
+INSERT INTO Utilisateurs(Nom, Prenom, Adresse, DateNaissance, IsStagiaire, IsAdministrateur)
+				VALUES ("Emmanuel", "Lagrange", "88, Rue Joseph Vernet 92220 BAGNEUX", "1984-02-12", 1, 0);
 
 CREATE TABLE Entreprises (
 	Id					int(6)			PRIMARY KEY AUTO_INCREMENT,
@@ -28,7 +35,25 @@ CREATE TABLE Entreprises (
 ) ENGINE = InnoDB;
 
 INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
-				VALUES ("Quinton Hazell", "24 rue des champs", "59120 Lomme", "2001-01-01", "2005-06-01", 1);
+				VALUES ("Quinton Hazell", "94, rue de Geneve", "80000 AMIENS", "2001-01-01", "2005-06-01", 1);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Lab Photo pro", "15, place Stanislas", "92000 NANTERRE", "2005-08-01", "2010-03-08", 1);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Food Giant", "42, rue de Geneve", "80000 AMIENS", "2001-01-01", "2009-06-01", 2);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Pantry Pride", "15, place Stanislas", "92000 NANTERRE", "2010-05-02", "2014-07-09", 2);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Record Town", "61, rue des six frères Ruellan", "57200 SARREGUEMINES", "2001-01-01", "2005-06-01", 3);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Magna Gases", "95, Rue Hubert de Lisle", "39000 LONS-LE-SAUNIER", "2005-09-01", "2008-08-25", 3);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Mr Fables", "49, avenue du Marechal Juin", "50000 SAINT-LÔ", "2009-06-15", "2012-03-06", 3);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Dynatronics Accessories", "19, Avenue Millies Lacroix", "78990 ÉLANCOURT", "2013-01-08", "2016-04-20", 3);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Titania", "93, rue du Faubourg National", "94320 THIAIS", "2001-01-01", "2005-06-01", 4);
+INSERT INTO Entreprises(Nom, Adresse, Ville, DateEntree, DateSortie, IdUtilisateur)
+				VALUES ("Liberal", "99, Chemin Des Bateliers", "49000 ANGERS", "2012-12-10", "2016-08-08", 4);
 
 CREATE TABLE Certifications (
 	Id				int(6)	PRIMARY KEY AUTO_INCREMENT,
@@ -36,6 +61,8 @@ CREATE TABLE Certifications (
 	IdFormation		int(6)	REFERENCES Formations(Id),
 	IdMetier		int(6)	REFERENCES Metiers(Id)
 ) ENGINE = InnoDB;
+
+
 
 CREATE TABLE Formations (
 	Id					int(6)	PRIMARY KEY AUTO_INCREMENT,
--- src/com/vianney/servlet/Index.java
+++ src/com/vianney/servlet/Index.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/")
+@WebServlet( urlPatterns = "/" )
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +26,7 @@ public class Index extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF//index.jsp").forward(request, response);
 	}
 
 	/**
