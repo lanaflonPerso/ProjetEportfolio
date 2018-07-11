1adf6a495cc5ba058073815ef548bcc5ab6c4910
vianneyba <vianneyba@free.fr>
2018-07-11 23:52:22 +0200

    amelioration FormStagiaire

--- src/com/vianney/dao/UtilisateurDao.java
+++
@@ -1,9 +0,0 @@
-package com.vianney.dao;
-
-import com.vianney.Utilisateur;
-
-public interface UtilisateurDao {
-	
-	void creer( Utilisateur utilisateur ) throws DAOException;
-	Utilisateur trouver( String email ) throws DAOException;
-}
